5f9982e8659deec0b00786003253332497f564bd
MadG0blin359 <shawaizshahid312+github@gmail.com>
2026-07-26 03:32:37 +0500

    add/ Task Data Repository

---
+++ todo-api/src/repositories/task.repository.js
@@ -0,0 +1,49 @@
+const tasks = [
+  {
+    id: 1,
+    title: "First Assignment",
+    done: true,
+  },
+  {
+    id: 2,
+    title: "Second Assignment",
+    done: true,
+  },
+  {
+    id: 3,
+    title: "Third Assignment",
+    done: false,
+  },
+];
+
+class TaskRepository {
+  findAll() {
+    return tasks;
+  }
+
+  findById(id) {
+    return tasks.findIndex((t) => t.id === id);
+  }
+
+  create(newTask) {
+    tasks.push(newTask);
+    return newTask;
+  }
+
+  update(idx, updateData) {
+    tasks[index] = {
+      ...tasks[index],
+      ...(updateData.title !== undefined && { title: updateData.title }),
+      ...(updateData.done !== undefined && { done: updateData.done }),
+    };
+
+    // Return the freshly updated object
+    return tasks[index];
+  }
+
+  delete(index) {
+    tasks.splice(index, 1);
+  }
+}
+
+export default new TaskRepository();
