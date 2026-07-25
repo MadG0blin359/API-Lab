c59936e108c6ddfc6bfe09fa308cc9f0db65c8fb
MadG0blin359 <shawaizshahid312+github@gmail.com>
2026-07-26 00:56:13 +0500

    refactor/ routes

---
+++ todo-api/src/routes/meta.routes.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import * as metaController from "../controllers/meta.controller";
+
+const router = Router();
+
+router.get("/", metaController.getAPIEndpoints);
+router.get("/health", metaController.getServerHealth);
+
+module.exports = router;
--- todo-api/src/routes/task.routes.js
+++ todo-api/src/routes/task.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import * as taskController from "../controllers/taskController.js";
+import * as taskController from "../controllers/task.controller.js";
 
 const router = Router();
 
