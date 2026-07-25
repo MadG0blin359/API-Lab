215c86f77fe174658d640e16305544d486889677
MadG0blin359 <shawaizshahid312+github@gmail.com>
2026-07-26 03:33:39 +0500

    fix/ Global Error Handling

--- todo-api/server.js
+++ todo-api/server.js
@@ -1,5 +1,13 @@
 import createApp from "./src/app.js";
 
+// Global Synchronous Code Error Handler
+process.on("uncaughtException", (err) => {
+  console.log("UNCAUGHT EXCEPTION! 💥 Shutting down...");
+  console.log(err.name, err.message);
+  // Shutdown immediately
+  process.exit(1);
+});
+
 const app = createApp();
 const PORT = process.env.PORT || 3000;
 
@@ -8,4 +16,14 @@ app.listen(PORT, () => {
     `Server is running in ${process.env.NODE_ENV || "development"} mode on port ${PORT}...`,
   );
   console.log(`Swagger UI available at http://localhost:${PORT}/docs`);
+});
+
+// Global Asynchronous Code Error Handler
+process.on("unhandledRejection", (err) => {
+  console.log("UNHANDLED REJECTION! 💥 Shutting down...");
+  console.log(err.name, err.message);
+  // Shutdown gracefully
+  server.close(() => {
+    process.exit(1);
+  });
 });
