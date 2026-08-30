e01ef6999db5bd8680c0ae89d6ed4c45351c037e
MadG0blin359 <shawaizshahid312+github@gmail.com>
2026-08-30 15:27:16 +0500

    routes: require auth on all task routes, add /stats endpoint

--- task-api-postgres/src/routes/task.routes.js
+++ task-api-postgres/src/routes/task.routes.js
@@ -1,10 +1,17 @@
 import { Router } from "express";
 import { validateTaskID } from "../validators/task.params.validator.js";
 import * as taskController from "../controllers/task.controller.js";
+import { validateAccessToken } from "../validators/auth.validator.js";
+import { authenticateUser } from "../middlewares/authenticate.user.js";
 
 const router = Router();
 
+// All task routes require authentication
+router.use(validateAccessToken, authenticateUser);
+
 router.param("id", validateTaskID);
+
+router.get("/stats", taskController.getTaskStats);
 
 router
   .route("/")
